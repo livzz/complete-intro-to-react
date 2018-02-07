8a5b896dd2e02d434928c4583ee9bb7ca7352149
Alibh <alibh619@gmail.com>
2018-02-07 15:02:30 +0530

    My comit and config

---
+++ js/Search.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Search = () => <h1>This is a search</h1>;
+
+export default Search;
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  context: __dirname,
-  entry: './js/ClientApp.jsx',
-  devtool: 'cheap-eval-source-map',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+    context: __dirname,
+    entry: "./js/ClientApp.jsx",
+    devtool: "cheap-eval-source-map",
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    devServer: {
+        publicPath: "/public/"
+    },
+    stats: {
+        colors: true,
+        reasons: true,
+        chunks: true
+    },
+    module: {
+        rules: [{
+                enforce: "pre",
+                test: /\.jsx?$/,
+                loader: "eslint-loader",
+                exclude: "/node_modules/",
+                options: {
+                    fix: true
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 };
