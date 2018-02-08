91a7690109b4f7b382dc15cf62b95dfd62bc57f0
Alibh <alibh619@gmail.com>
2018-02-08 11:45:29 +0530

    Search Funtionality added with Filtering

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => (
   <div className="landing">
     <h1> svideo </h1>
     <input type="text" placeholder="Search" />
-    <a> Browse File </a>
+    <Link to="/search"> Browse File </Link>
   </div>
 );
 
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import preload from "../data.json";
+import ShowCard from "./ShowCard";
 
-const Search = () => <h1>This is a search</h1>;
+class Search extends Component {
+  state = {
+    searchTerm: ""
+  };
+
+  handleSerachTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            onChange={this.handleSerachTermChange}
+            value={this.state.searchTerm}
+            placeholder="search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} show={show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
